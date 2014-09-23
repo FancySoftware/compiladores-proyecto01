d5da59eb0695d8d939afb9f733e7dcb4da25fbc6
José Manuel Snyder <snyder@ciencias.unam.mx>
2014-09-23 15:39:45 -0500

    Un dedent a la vez

--- src/pyprueba1.py
+++ src/pyprueba1.py
@@ -1,3 +1,9 @@
-print "hola"
- print "ksuhf"
-  print "jyvf"
+#Comentario Inicial
+if 3 > 1:
+	x = -4
+	while x < 2:#COMENTARIO
+		x+=1
+		#COMENTARIO
+    	print x
+else: #COMENTARIO FALSO
+   x = "Hello 10 world 15"
