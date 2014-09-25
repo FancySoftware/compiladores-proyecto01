5faf592181e19b8be58c21864ce6ffef4701a8f3
José Manuel Snyder <snyder@ciencias.unam.mx>
2014-09-24 23:08:44 -0500

    Archivos para manejo de keywords

---
+++ src/keyword.cpp
@@ -0,0 +1,41 @@
+#include <string>
+#include "pylex_parser.h"
+#include "keyword.h"
+using namespace std;
+
+yytokentype getTokenFromKeyword(string kyeword){
+    if (keyword.compare("print")) return PRINT;
+    if (keyword.compare("False")) return FALSE;
+    if (keyword.compare("class")) return CLASS;
+    if (keyword.compare("finally")) return FINALLY;
+    if (keyword.compare("is")) return IS;
+    if (keyword.compare("return")) return RETURN;
+    if (keyword.compare("None")) return NONE;
+    if (keyword.compare("continue")) return CONTINUE;
+    if (keyword.compare("for")) return FOR;
+    if (keyword.compare("lambda")) return LAMBDA;
+    if (keyword.compare("try")) return TRY;
+    if (keyword.compare("True")) return TRUE;
+    if (keyword.compare("def")) return DEF;
+    if (keyword.compare("from")) return FROM;
+    if (keyword.compare("nonlocal")) return NONLOCAL;
+    if (keyword.compare("while")) return WHILE;
+    if (keyword.compare("and")) return AND;
+    if (keyword.compare("del")) return DEL;
+    if (keyword.compare("global")) return GLOBAL;
+    if (keyword.compare("not")) return NOT;
+    if (keyword.compare("with")) return WITH;
+    if (keyword.compare("as")) return AS;
+    if (keyword.compare("elif")) return ELIF;
+    if (keyword.compare("if")) return IF;
+    if (keyword.compare("or")) return OR;
+    if (keyword.compare("yield")) return YIELD;
+    if (keyword.compare("assert")) return ASSERT;
+    if (keyword.compare("else")) return ELSE;
+    if (keyword.compare("import")) return IMPORT;
+    if (keyword.compare("pass")) return PASS;
+    if (keyword.compare("break")) return BREAK;
+    if (keyword.compare("except")) return EXCEPT;
+    if (keyword.compare("in")) return IN;
+    if (keyword.compare("raise")) return RAISE;
+}
---
+++ src/keyword.h
@@ -0,0 +1,5 @@
+#include <string>
+#include "pylex_parser.h"
+using namespace std;
+
+yytokentype getTokenFromKeyword(string);
