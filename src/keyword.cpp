aa40f7211ca345b4ec8c4f7cb38d2f0aaa72fc35
nancy <nancy@nancy.(none)>
2014-09-26 16:37:26 -0500

    Quitamos keywords de python3

--- src/keyword.cpp
+++ src/keyword.cpp
@@ -5,20 +5,13 @@ using namespace std;
 
 yytokentype getTokenFromKeyword(string keyword){
     if (keyword.compare("print")) return PRINT;
-    if (keyword.compare("False")) return FALSE;
-    if (keyword.compare("class")) return CLASS;
-    if (keyword.compare("finally")) return FINALLY;
     if (keyword.compare("is")) return IS;
-    if (keyword.compare("return")) return RETURN;
-    if (keyword.compare("None")) return NONE;
+    if (keyword.compare("return")) return RETURN; 
     if (keyword.compare("continue")) return CONTINUE;
     if (keyword.compare("for")) return FOR;
     if (keyword.compare("lambda")) return LAMBDA;
-    if (keyword.compare("try")) return TRY;
-    if (keyword.compare("True")) return TRUE;
     if (keyword.compare("def")) return DEF;
     if (keyword.compare("from")) return FROM;
-    if (keyword.compare("nonlocal")) return NONLOCAL;
     if (keyword.compare("while")) return WHILE;
     if (keyword.compare("and")) return AND;
     if (keyword.compare("del")) return DEL;
@@ -30,7 +23,6 @@ yytokentype getTokenFromKeyword(string keyword){
     if (keyword.compare("if")) return IF;
     if (keyword.compare("or")) return OR;
     if (keyword.compare("yield")) return YIELD;
-    if (keyword.compare("assert")) return ASSERT;
     if (keyword.compare("else")) return ELSE;
     if (keyword.compare("import")) return IMPORT;
     if (keyword.compare("pass")) return PASS;
