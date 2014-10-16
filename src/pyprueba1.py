34878632a860164600deaef6000058c280757833
nancy <nancy@nancy.(none)>
2014-10-15 23:12:03 -0500

    simbolos

--- src/pyprueba1.py
+++ src/pyprueba1.py
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-def aja():
-	1
-=======
-print "cadena"
-print "otra mas"
-if 3 > 1:
-    x = 10
->>>>>>> b586bcc2a5d884737f12527e7535b03c00fd0a4e
-=======
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-x = "Codigo de prueba"
-avanza = True
-while avanza:
-    y += 1
-    if y == 8 :
-        print "Y es 8"
-        avanza = False
-    else:
-        print " esta es una cadenaa"
-        z = 10
-print "Termina el programa"
->>>>>>> 45eaa335d148480e0323e49b04d2ea0010c6c57f
+for x in range(0,10):
+	x =3
+"jiji" ola"
