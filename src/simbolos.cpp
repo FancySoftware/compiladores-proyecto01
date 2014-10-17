bd20ad1f3eedf63c7ab334a896040cbb72f0dae7
nancy <nancy@nancy.(none)>
2014-10-16 22:03:21 -0500

    simbolos

--- src/simbolos.cpp
+++ src/simbolos.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <unordered_map>
 #include <string>
+#include <vector>
+
 using namespace std;
 
 class Simbolos
@@ -31,30 +33,36 @@ public:
 
 class Simbolo: public Simbolos{
 
+	vector<unordered_map<string,Elemento>> vector_tablas;
 	int alcance_actual;
-	unordered_map<string, Elemento> tabla;
+	unordered_map<string, Elemento> tabla_actual;
 
 
 	/* Funcion para buscar un elemento en la tabla de simbolos*/
 	Elemento lookup(string name){
-		unordered_map<string,Elemento>::const_iterator iter = tabla.find(name); 
-		if(iter != tabla.end()){
-			return iter->second;
+		for (int i =alcance_actual-1; i>=0;i--){
+			unordered_map<string, Elemento>	tabla_aux = vector_tablas[i];
+			unordered_map<string,Elemento>::const_iterator iter = tabla_aux.find(name); 
+			if(iter != tabla_aux.end()){
+				return iter->second;
+			}
 		}
 	}
 	
 	/* Funcion para abrir un nuevo alcance en la tabla de simbolos */
 	void openScope(){
 		alcance_actual +=1;
+		unordered_map<string,Elemento> tabla_nva;
+		tabla_actual = tabla_nva;
 	}
 
 	/* Funcion que nos permite saber si name esta dentro del alcaance local, es decir el 
 	mas interno hasta el momento*/
 	bool declaredLocally(string name){
 		bool declarado = true;
-		unordered_map<string,Elemento>::const_iterator iter = tabla.find(name);
+		unordered_map<string,Elemento>::const_iterator iter = tabla_actual.find(name);
 		Elemento temp = iter->second;
-		if(temp.alcance != alcance_actual || iter == tabla.end()){
+		if(temp.alcance != alcance_actual || iter == tabla_actual.end()){
 			declarado = false;
 		}
 		return declarado;
