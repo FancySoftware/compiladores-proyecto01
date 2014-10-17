2308d37238df9fa66756cef6aa2fd8b1de91b892
José Manuel Snyder <snyder@ciencias.unam.mx>
2014-10-17 00:01:08 -0500

    pruebas

--- src/simbolos.cpp
+++ src/simbolos.cpp
@@ -5,22 +5,22 @@
 
 using namespace std;
 
+/*Creacion de la estructura para cada uno de los elementos de la tabla de simbolos*/
+struct Elemento{
+	int alcance;
+	string nombre;
+	string tipo;
+	/* Union para definir el valor de los elementos*/
+	union {
+		int number;
+		char cadena[];
+	};
+};
+
 class Simbolos
 {
 
 public:
-
-/*Creacion de la estructura para cada uno de los elementos de la tabla de simbolos*/
-	struct Elemento{
-		int alcance;
-		string nombre;
-		string tipo;
-		/* Union para definir el valor de los elementos*/
-		union {
-			int number;
-			char cadena[];
-		};
-	};
 
 /*Declaracion de los metodo que tenemos que implementar*/
 	virtual Elemento lookup(string name)=0;
@@ -28,34 +28,38 @@ public:
 	virtual void openScope()=0;
 	virtual void closeScope()=0;
 	virtual bool declaredLocally(string name)=0;
+	virtual void print()=0;
 
 };
 
 class Simbolo: public Simbolos{
-
+public:
 	vector<unordered_map<string,Elemento>> vector_tablas;
 	vector<unordered_map<string,Elemento>>::iterator it;
 
 	int alcance_actual;
 	unordered_map<string, Elemento> tabla_actual;
 
+	Simbolo() {
+		alcance_actual = 0;
+	}
 
 	/* Funcion para buscar un elemento en la tabla de simbolos*/
 	Elemento lookup(string name){
 		for (int i =alcance_actual-1; i>=0;i--){
 			unordered_map<string, Elemento>	tabla_aux = vector_tablas[i];
-			unordered_map<string,Elemento>::const_iterator iter = tabla_aux.find(name); 
+			unordered_map<string,Elemento>::const_iterator iter = tabla_aux.find(name);
 			if(iter != tabla_aux.end()){
 				return iter->second;
 			}
 		}
 	}
-	
+
 	/* funcion que inserta dentro de la tabla de simbolos del alcance actual un elemento*/
 	virtual void insert(string name, Elemento record){
-		if(!declaredLocally(name)){
+		//if(!declaredLocally(name)){
 			tabla_actual.insert({name,record});
-		}
+		//}
 	}
 
 	/* Funcion para abrir un nuevo alcance en la tabla de simbolos */
@@ -65,7 +69,7 @@ class Simbolo: public Simbolos{
 		tabla_actual = tabla_nva;
 		// Agregamos la nueva tabla del alcance a nuestro vector de tablas
 		it = vector_tablas.begin();
-		vector_tablas.insert(it,1,tabla_nva);		
+		vector_tablas.push_back(tabla_nva);
 	}
 
 	/* Cierra el alcance actual referido a la tabla de simbolos, regresa a un alcance exterior*/
@@ -74,7 +78,7 @@ class Simbolo: public Simbolos{
 		tabla_actual = vector_tablas[alcance_actual];
 	}
 
-	/* Funcion que nos permite saber si name esta dentro del alcaance local, es decir el 
+	/* Funcion que nos permite saber si name esta dentro del alcaance local, es decir el
 	mas interno hasta el momento*/
 	bool declaredLocally(string name){
 		bool declarado = true;
@@ -84,9 +88,28 @@ class Simbolo: public Simbolos{
 			declarado = false;
 		}
 		return declarado;
-	}	
+	}
+
+	/*Funcion para imprimir la tabla, usada para las pruebas*/
+	void print() {
+		/*for( it = vector_tablas.begin(); it != vector_tablas.end(); ++it) {
+			unordered_map<string, Elemento> for_print = *it;
+			for ( auto local_it = for_print.begin(); local_it!= for_print.end(); ++local_it )
+      			cout << " " << local_it->first << ":" << local_it->second;
+      		cout << endl;
+		}*/
+	}
 };
 
 int main(){
+	Simbolo *prueba = new Simbolo();
+	prueba->openScope();
+	struct Elemento test;
+	test.alcance = 0;
+	test.nombre = "equis";
+	test.tipo = "int";
+	test.number = 10;
+	prueba->insert("equis",test);
+	//prueba->print();
 	return 0;
 }
